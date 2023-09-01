265a88b8f8b48043c64e2e196c57d6dd22cf32d9
Joseph Clyde Mar <clydemar234@gmail.com>
2023-09-01 22:19:16 +0800

    Add error logging to a text file feature

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os, time, datetime, win32clipboard
 
-img_directory = 'clip'
+clip_record_directory = 'clip'
+clip_record_file_names = [
+    'clip_record',
+    'clip-record-IT-videos-special',
+    'clip-record-TCP-and-Sockets-videos-special',
+    'clip-record-various-videos'
+    ]
+clip_record_file = clip_record_file_names[0]
+
 try:
-    os.mkdir(img_directory)
-    print(f'{img_directory} is created...')
+    os.mkdir(clip_record_directory)
+    print(f'{clip_record_directory} is created...')
 except FileExistsError as e:
     print(e)
 
@@ -12,17 +20,23 @@ latest_clipboard_data = ''
 old_clipboard_data = latest_clipboard_data
 while True:
     win32clipboard.OpenClipboard()
+    date_copied = datetime.datetime.today()
     try:
         latest_clipboard_data = win32clipboard.GetClipboardData()
         if old_clipboard_data != latest_clipboard_data:
-            date_copied = datetime.datetime.today()
-            with open(f'{img_directory}\clip_record.txt', 'a') as f:
+            
+            with open(f'{clip_record_directory}\{clip_record_file}.txt', 'a') as f:
                 f.write(f'[(+)][{date_copied}]:(+):{latest_clipboard_data}\n')
             print(f'[{date_copied}]: {latest_clipboard_data}')
             old_clipboard_data = latest_clipboard_data
     
     except TypeError as clip_exception:
         print('Clip Board Data Access Error: ', clip_exception)
+    except Exception as general_exception:
+        print('General Error: ', general_exception)
+        with open('error-log-folder\error-log-file.txt', 'a') as f:
+            f.write(f'[{date_copied}]: {general_exception}\n')
+        break
     win32clipboard.CloseClipboard()
     time.sleep(0.1)
 
